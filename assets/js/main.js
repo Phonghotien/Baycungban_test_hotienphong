bb5ddf7bf3fb3b06a919e380b6f12b68bb4d8732
Ho Phong <hotienphong120699@gmail.com>
2022-04-06 23:47:40 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,3 +90,19 @@ $('input[type="radio"]').mousedown(function() {
         });
     }
 });
+// menu
+const menu = document.querySelector(".header__nav");
+const menuButton = document.getElementById("navbar__icons");
+const overlay = document.querySelector("#overlay")
+
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+    overlay.classList.toggle("show")
+});
+overlay.addEventListener('click', () => {
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+    overlay.classList.toggle("show")
+});
