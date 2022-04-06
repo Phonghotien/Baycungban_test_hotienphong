482c35cd0e2eb9588581a992b5ecb607bd45a038
Hồ Tiên Phong <101172758+Phonghotien@users.noreply.github.com>
2022-04-07 00:33:57 +0700

    Update main2.js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,3 +1,11 @@
+//swap
+$('.booking__convert').mousedown('click', function() {
+    var $first = $('.booking__form').find('.subbox').first();
+    var $last = $('.booking__form').find('.subbox').last();
+
+    $('.booking__convert').after($first);
+    $('.booking__convert').before($last);
+});
 // menu
 const menu = document.querySelector(".header__nav");
 const menuButton = document.getElementById("navbar__icons");
